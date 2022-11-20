f2dd5832b1e5a1a6862d904ceee1e3813b93b87a
triciahughes <hughes.a.tricia@gmail.com>
2022-11-19 20:46:55 -0800

    commit tree c18d7061fd49cb3b63cdf1fed89bdc8498c99c62

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+};
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+}
+
+function appendCat(name) {
+    //const copyCats = [...cats];
+    //copyCats.push(name);
+    return [...cats, name];
+}
+
+function prependCat(name) {
+    return [name, ...cats];
+}
+
+function removeLastCat() {
+    return [...cats.slice(0,-1)];
+}
+
+function removeFirstCat() {
+    return [...cats.slice(1)];
+}
